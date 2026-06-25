3870e1d3dde15ef10c70e0235fa35794987081fa
halimahmmahfuzah-afk <halimah.mmahfuzah@gmail.com>
2026-06-25 15:14:39 +0800

    Selesai Bagian 1: Stream dan File

---
+++ IOStreamFile1/src/bagian1/berkas/Latihan1.java
@@ -0,0 +1,16 @@
+package bagian1.berkas;
+
+import java.io.File;
+
+public class Latihan1 {
+    public static void main(String[] args) {
+        File berkas = new File("laporan.txt");
+
+        if (berkas.exists()) {
+            System.out.println("Berkas ada.");
+            System.out.println("Ukuran berkas: " + berkas.length() + " byte");
+        } else {
+            System.out.println("Berkas tidak ada.");
+        }
+    }
+}
